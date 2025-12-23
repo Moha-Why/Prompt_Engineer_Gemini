c43898286f06f168d0addf8ee85dd133865729ad
Mohamed <me878783@gmail.com>
2025-12-23 20:49:27 +0200

    hello

--- src/server.js
+++ src/server.js
@@ -52,5 +52,5 @@ app.post("/", async (req, res) => {
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server is running at port ${port}`)
+    console.log(`Server is running at port ${PORT}`)
 })
